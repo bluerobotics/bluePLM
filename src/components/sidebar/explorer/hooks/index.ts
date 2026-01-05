3d251cffe3d770102fb126db8bfad7cf67ad6c60
Eldin Miller-Stead <49656283+eldinmiller@users.noreply.github.com>
2026-01-05 10:41:35 -0800

    refactor(api): add core types, errors, and config

--- src/components/sidebar/explorer/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-// Barrel export for explorer hooks
-export { useTreeExpansion } from './useTreeExpansion'
-export { useTreeDragDrop } from './useTreeDragDrop'
-export { useTreeKeyboardNav } from './useTreeKeyboardNav'
-export { useVaultTree } from './useVaultTree'
